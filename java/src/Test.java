7bceea4e6d2cba5b259ecfca333459a58d0f81c8
yyquan <531873600@qq.com>
2021-03-23 10:05:22 +0800

    update 面试资料

--- java/src/Test.java
+++ java/src/Test.java
@@ -1,20 +1,67 @@
 import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class Test {
 
-    public static void main(String[] args) {
+    private static AtomicInteger atomicInt = new AtomicInteger(100);
+    private static AtomicStampedReference atomicStampedRef = new AtomicStampedReference(100, 0);
 
-//        char c = '中';
-        byte[] bytes = new byte[0];
-        try {
-            bytes = "中".getBytes("utf-16");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        System.out.println(bytes.length);
-        for (byte b : bytes) {
-            System.out.print(Integer.toHexString(Byte.toUnsignedInt(b)));
-            System.out.print("");
-        }
+    public static void main(String[] args) throws InterruptedException {
+        Thread intT1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                atomicInt.compareAndSet(100, 101);
+                atomicInt.compareAndSet(101, 100);
+            }
+        });
+
+        Thread intT2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                }
+                boolean c3 = atomicInt.compareAndSet(100, 101);
+                System.out.println(c3); // true
+            }
+        });
+
+        intT1.start();
+        intT2.start();
+        intT1.join();
+        intT2.join();
+
+        Thread refT1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                }
+                atomicStampedRef.compareAndSet(100, 101, atomicStampedRef.getStamp(), atomicStampedRef.getStamp() + 1);
+                atomicStampedRef.compareAndSet(101, 100, atomicStampedRef.getStamp(), atomicStampedRef.getStamp() + 1);
+            }
+        });
+
+        Thread refT2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int stamp = atomicStampedRef.getStamp();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+                boolean c3 = atomicStampedRef.compareAndSet(100, 101, stamp, stamp + 1);
+                System.out.println(c3); // false
+            }
+        });
+
+        refT1.start();
+        refT2.start();
+        refT1.join();
+        refT2.join();
     }
 }
